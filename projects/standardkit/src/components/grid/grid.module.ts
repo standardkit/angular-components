4527290e30bc0597a70871e424950f6021049a02
Coen Mooij <coenmooij@gmail.com>
2021-03-03 20:06:26 +0100

    Add grid components

--- projects/standardkit/src/components/grid/grid.module.ts
+++ projects/standardkit/src/components/grid/grid.module.ts
@@ -1,9 +1,18 @@
 import { NgModule } from '@angular/core';
+import { SkColumnComponent } from './column/column.component';
+import { SkContainerComponent } from './container/container.component';
+import { SkRowComponent } from './row/row.component';
+
+const COMPONENTS = [
+  SkColumnComponent,
+  SkContainerComponent,
+  SkRowComponent
+];
 
 @NgModule({
   imports: [],
-  declarations: [],
-  exports: []
+  declarations: COMPONENTS,
+  exports: COMPONENTS
 })
 export class SkGridModule {
 }
