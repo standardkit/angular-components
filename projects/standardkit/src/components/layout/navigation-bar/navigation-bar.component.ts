209fcd0738461f7bdf8e96d40edb000b8465a91f
Coen Mooij <coenmooij@gmail.com>
2021-03-04 16:32:19 +0100

    add hostbinding for navigation bar container

--- projects/standardkit/src/components/layout/navigation-bar/navigation-bar.component.ts
+++ projects/standardkit/src/components/layout/navigation-bar/navigation-bar.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, HostBinding, Input } from '@angular/core';
 import { CaseType } from '../../../types/case.type';
 import { FillType } from '../../../types/fill.type';
 import { SizeType } from '../../../types/size.type';
@@ -8,6 +8,8 @@ import { SizeType } from '../../../types/size.type';
   templateUrl: 'navigation-bar.component.html'
 })
 export class SkNavigationBarComponent {
+  @HostBinding('class') class = 'navigation-bar__container';
+
   @Input() padding?: SizeType | string;
   @Input() fill?: FillType | string;
   @Input() size?: SizeType | string;
