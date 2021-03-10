4abc66f2367f631751eab38dac37210efef4c994
Coen Mooij <coenmooij@gmail.com>
2021-03-10 14:44:56 +0100

    refactor color to type in navigation bar and remove deprecated usage of styles

--- projects/standardkit/src/types/color.type.ts
+++ projects/standardkit/src/types/color.type.ts
@@ -1,1 +1,4 @@
-export type ColorType = 'primary' | 'secondary' | 'contrast';
+import { AlertLevelType } from '@standardkit/angular-components';
+
+// TODO : Make complete colors
+export type ColorType = AlertLevelType | 'primary' | 'secondary' | 'contrast';
