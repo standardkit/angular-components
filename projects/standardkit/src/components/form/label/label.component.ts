3826c37b7c72c882f1dc8ab769659cb8e8ada3d0
Coen Mooij <coenmooij@gmail.com>
2021-03-19 10:02:21 +0100

    add config options for label

--- projects/standardkit/src/components/form/label/label.component.ts
+++ projects/standardkit/src/components/form/label/label.component.ts
@@ -1,4 +1,6 @@
 import { Component, Input } from '@angular/core';
+import { SkConfigurationService } from '../../../configuration/configuration.service';
+import { SizeType } from '../../../types/size.type';
 
 @Component({
   selector: 'sk-label',
@@ -6,4 +8,19 @@ import { Component, Input } from '@angular/core';
 })
 export class SkLabelComponent {
   @Input() fieldId?: string = 'input';
+
+  @Input() padding?: SizeType | string;
+  @Input() margin?: SizeType | string;
+  @Input() size?: SizeType | string;
+
+  defaultMargin?: SizeType | string;
+  defaultPadding?: SizeType | string;
+  defaultSize?: SizeType | string;
+
+  constructor(private configurationService: SkConfigurationService) {
+    const configuration = configurationService.get();
+    this.defaultSize = configuration?.label?.size ?? configuration?.size;
+    this.defaultMargin = configuration?.label?.margin ?? configuration?.margin;
+    this.defaultPadding = configuration?.label?.padding ?? configuration?.padding;
+  }
 }
